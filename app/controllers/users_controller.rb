7e9e18d2df7fca27cc336a6e0de3811381f7e37b
sayumio <s83.oshima.sayumi@gmail.com>
2020-10-04 13:46:08 +0900

    userコントローラーの記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :is_password_authenticated, :family_name, :first_name, :kana_family_name, :kana_first_name, :birth_date)
+  end
+
 end
