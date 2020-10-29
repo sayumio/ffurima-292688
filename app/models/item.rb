e28ed8fe6d9fc0f64e78385dffa5a8188e6909a1
sayumio <s83.oshima.sayumi@gmail.com>
2020-10-29 15:14:45 +0900

    バリテーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to       :user
+  has_one          :order
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
@@ -13,4 +15,16 @@ class Item < ApplicationRecord
 
   #選択が"--"では投稿できない
   validates :category_id, :status_id, :freight_id, :ship_region_id, :ship_date_id, numericality: { other_than: 1 }
+
+  #40文字以内でないと投稿できない
+  validates :name, length: { maximum: 40 }
+
+  #1000文字以内でないと投稿できない
+  validates :description, length: { maximum: 1000 }
+
+  #300〜9999999以内でないと投稿できない
+  validates :price, length: { in: 300..9999999 }
+
+  #数字でないと投稿できない
+  validates :price, :numericality => { :only_interger => true }
 end
