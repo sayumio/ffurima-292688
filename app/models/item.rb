3b3004b820d819e42705acb81818171a58a4c5f7
sayumio <s83.oshima.sayumi@gmail.com>
2020-10-29 14:11:49 +0900

    商品状態

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :freight
+  belongs_to_active_hash :status
 
   #空の状態で投稿できない
-  validates :category, :freight, presence: true
+  validates :category, :status, :freight, presence: true
 
   #選択が"--"では投稿できない
-  validates :category_id, :freight_id,  numericality: { other_than: 1 }
+  validates :category_id, :status_id, :freight_id,  numericality: { other_than: 1 }
 end
