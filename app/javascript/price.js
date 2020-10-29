766f0a5d017d9f2adbdc3e9be1cdafbed10b8b43
sayumio <s83.oshima.sayumi@gmail.com>
2020-10-29 20:40:12 +0900

    金額自動計算

---
+++ app/javascript/price.js
@@ -0,0 +1,19 @@
+function price() {
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const Profit = document.getElementById("profit");
+
+  itemPrice.addEventListener('input', function() {
+    let formData = itemPrice.value;
+    let fee = Math.floor(formData * 0.1);
+    let sales = formData - fee;
+    fee = String(fee).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    addTaxPrice.innerHTML = fee;
+
+    sales = String(sales).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    Profit.innerHTML = sales;
+
+  });
+}
+
+window.addEventListener('load', price)
